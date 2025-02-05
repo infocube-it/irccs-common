ae4e2ebc2cd3e488aeec9253e375c1f82165d10b
Antonio Russo <antonio@MacBook-Pro-di-Antonio.local>
2025-02-05 13:52:46 +0100

    Fixed Log dependency

--- hapi-devservice/deployment/src/main/java/org/hapi/devservice/deployment/HapiDevServicesProcessor.java
+++ hapi-devservice/deployment/src/main/java/org/hapi/devservice/deployment/HapiDevServicesProcessor.java
@@ -7,8 +7,6 @@ import io.quarkus.deployment.builditem.FeatureBuildItem;
 import io.quarkus.deployment.builditem.LaunchModeBuildItem;
 import io.quarkus.deployment.dev.devservices.GlobalDevServicesConfig;
 import io.quarkus.runtime.LaunchMode;
-import jakarta.inject.Inject;
-import org.jboss.logging.Logger;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -16,16 +14,13 @@ import java.util.Optional;
 
 /** @noinspection ALL*/
 public class HapiDevServicesProcessor {
-    
+
     private static final String FEATURE = "hapi-devservice";
 
     private static final int HAPI_PORT = 8080;
     private static final String DEV_SERVICE_LABEL = "quarkus-dev-service-hapi";
     private static final ContainerLocator hapiDevModeContainerLocator = new ContainerLocator(DEV_SERVICE_LABEL, HAPI_PORT);
     static volatile DevServicesConfig capturedDevServicesConfiguration = new DevServicesConfig();
-
-    @Inject
-    Logger logger;
 
     @BuildStep
     FeatureBuildItem feature() {
@@ -39,10 +34,7 @@ public class HapiDevServicesProcessor {
                 capturedDevServicesConfiguration.serviceName,
                 capturedDevServicesConfiguration.shared,
                 LaunchMode.current());
-
-        logger.debug(capturedDevServicesConfiguration.serviceName);
-        logger.debug(capturedDevServicesConfiguration.shared);
-
+        
         if (maybeContainerAddress.isPresent()) {
             // Container exists, reuse it
             ContainerAddress containerAddress = maybeContainerAddress.get();
